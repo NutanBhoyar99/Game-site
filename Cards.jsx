023eb46cff635f499f6674b41cec9bf9e39dab4a
NutanBhoyar99 <54523680+NutanBhoyar99@users.noreply.github.com>
2021-07-07 19:26:04 +0530

    Add files via upload

---
+++ Cards.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Cards(props){
+    return(
+      
+  <div className="cards">
+    <div className ="card">
+      <img src ={props.imgsrc} alt="My_img" className="card__img" />
+      <div className="card__info">
+        <span className="card__category">{props.sname}</span>
+        <h3 className="card__title"> {props.title}</h3>
+        <a href={props.link} target="_blank">
+          <button>Play Now</button>
+        </a>
+  
+      </div>
+  
+    </div>
+  
+  </div>
+
+  
+  
+    )
+  }
+  export default Cards;
---
+++ Sdata.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import img1 from "./Images/supermario.jpg";
+const Sdata = [
+
+    {
+        imgsrc: "https://tse3.mm.bing.net/th?id=OIP.BvriPJra1wbseYngRo0dpwHaHa&pid=Api&P=0&w=300&h=300",
+        title: "A Game Original Series",
+        sname: "Super Mario",
+        link: "https://www.primevideo.com/?ref_=dvm_pds_cpb_in_as_s_b_d492B|m_RrPK44ssc_c"
+    },
+    {
+       imgsrc:"https://tse2.mm.bing.net/th?id=OIP.ofINYlSxB_GqgBKOq1PYVQHaEK&pid=Api&P=0&w=319&h=180", 
+        title:"A Game Original Series",
+        sname:"Pac-Man" ,
+         link:"https://www.primevideo.com/?ref_=dvm_pds_cpb_in_as_s_b_d492B|m_RrPK44ssc_c"
+    },
+    {
+        imgsrc:"https://tse3.mm.bing.net/th?id=OIP.Ac420gcTy91Ncq39dKFGgQHaE5&pid=Api&P=0&w=266&h=176" ,
+        title:"A Game Original Series",
+        sname:"Infinite Mario Bros" ,
+        link:"https://www.primevideo.com/?ref_=dvm_pds_cpb_in_as_s_b_d492B|m_RrPK44ssc_c"
+    },
+
+    {
+        imgsrc:"https://tse4.mm.bing.net/th?id=OIP.wyhrqsmzYnETApE3ZIO_gAHaHa&pid=Api&P=0&w=300&h=300" ,
+         title:"A Game Original Series",
+        sname:"Wordpress" ,
+        link:"https://www.primevideo.com/?ref_=dvm_pds_cpb_in_as_s_b_d492B|m_RrPK44ssc_c"
+    },
+
+    {
+        imgsrc:"https://tse4.mm.bing.net/th?id=OIP.-rI4rxC3VTlpIhf7e75fdgHaEo&pid=Api&P=0&w=286&h=180" ,
+        title:"A Game Original Series",
+        sname:"Wolftenstein " ,
+        link:"https://www.primevideo.com/?ref_=dvm_pds_cpb_in_as_s_b_d492B|m_RrPK44ssc_c"
+    }
+
+
+]
+export default Sdata;
